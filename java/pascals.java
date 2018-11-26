49a6d942b844d0f0982b2d0f4dd85c3a64ce366f
Jack Williams <44446909+jackwilliams2480@users.noreply.github.com>
2018-11-26 14:20:03 -0500

    Update for extra array tests

--- java/pascals.java
+++ java/pascals.java
@@ -3,10 +3,12 @@ public class pascals{
     public static int array[][];
 
     public static void main(){
-        pacal(8);
+        pascal(8);
+        pascal2nd(8);
+        //pascal3rd(8);
     }
 
-    public static void pacal(int scale){
+    public static void pascal(int scale){
         array = new int[scale+1][scale*2+1];
         array[0][scale] = 1;
         array[1][scale-1] = 1;
@@ -47,5 +49,77 @@ public class pascals{
         }
 
     }
+    
+    public static void pascal2nd(int scale){
+        array = new int[scale+1][scale+1];
+        array[0][0] = 1;
+        array[1][0] = 1;
+        array[1][1] = 1;
+        int row = 2;int colum = 0;
+        int algRow = row;int algColum1 = colum;int algColum2 = colum;
+        int increment = 1;
+        while(array[scale][array[0].length-1] == 0){
+            //int tempRow = row;
+            int tempColum = colum;
+            algRow = row - 1;
+            array[row][colum] = 1;
+            for(int i = 0;i <= increment;i++){
+                colum++;
+                algColum1 = colum;
+                algColum2 = colum-1;
+                if(row < 0 || colum < 0 || colum > 8 || row > 8
+                || algRow < 0 || algColum1 < 0 || algColum1 > 8 || algRow > 8
+                || algColum2 < 0 || algColum2 > 8)break;
+                array[row][colum] = (array[algRow][algColum1] + array[algRow][algColum2]);
+            }
+            increment++;
+            array[row][colum] = 1;
+            row++;colum = tempColum;
+        }
 
+        //Print out array
+        for(int i = 0;i <= array.length-1;i++){
+            for(int a = scale*2;a>=0;a--){
+                System.out.print(" ");
+            }
+            scale--;
+            for(int j = 0;j <= array[0].length-1;j++){
+                if(array[i][j] != 0 & i < 4)System.out.print(array[i][j] + "  ");
+                else if(array[i][j] != 0)System.out.print(array[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }    
+
+    public static void pascal3rd(int scale){
+        array = new int[scale+1][scale/2+1];
+        array[0][0] = 1;
+        array[1][0] = 1;
+        int row = 2;int colum = 0;
+        int algRow = row;int algColum1 = colum;int algColum2 = colum;
+        int increment = 1;
+        while(array[scale][array[0].length-1] == 0){
+            //int tempRow = row;
+            int tempColum = colum;
+            algRow = row - 1;
+            array[row][colum] = 1;
+            for(int i = 1;i <= increment/2;i++){
+                colum++;
+                algColum1 = colum;
+                algColum2 = colum-1;
+                if(row < 0 || colum < 0 || colum > 8 || row > 8
+                || algRow < 0 || algColum1 < 0 || algColum1 > 8 || algRow > 8
+                || algColum2 < 0 || algColum2 > 8)break;
+                array[row][colum] = (array[algRow][algColum1] + array[algRow][algColum2]);
+            }
+            increment++;
+            array[row][colum] = 1;
+            row++;colum = tempColum;
+        }
+
+        //Print out array
+        for(int i[] : array){
+            System.out.println(Arrays.toString(i));
+        }
+    } 
 }
